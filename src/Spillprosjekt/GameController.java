a92d28815e853f732fd8d62338a08310e5d0da99
Kristian <Kristian@10.22.51.70>
2017-04-04 15:53:25 +0200

    Forsøk på fix for feilsøking

---
+++ src/Spillprosjekt/GameController.java
@@ -0,0 +1,22 @@
+package spillprosjekt;
+
+
+
+public class GameController extends Application {
+	
+	public void main(String[] args)  {
+		launch(args);
+	}
+	
+	poublic void start(Stage stage) {
+		stage.setTitle("testing 123")
+		stage.
+		
+		
+		
+	}
+	
+	
+	 
+	
+}
